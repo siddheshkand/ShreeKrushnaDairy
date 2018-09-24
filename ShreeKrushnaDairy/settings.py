4f780f5c0afd134feecda2e2fabf723f6afaea80
siddheshkand <siddkand@gmail.com>
2018-09-24 17:01:11 +0530

    template fixed

--- ShreeKrushnaDairy/settings.py
+++ ShreeKrushnaDairy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ShreeKrushnaDairy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Home/templates'],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
