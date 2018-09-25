780b894757a0f8ce945d010a6d494aad53c634c9
siddheshkand <siddkand@gmail.com>
2018-09-25 23:23:11 +0530

    graph added

--- Dairy/views.py
+++ Dairy/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.http import *
-from django.shortcuts import render_to_response, redirect
-from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from Home.models import UserForm
 
 
 # Create your views here.
 @login_required(login_url='/dairy/login')
 def dairy_home(request):
-    return render(request, "Dairy/dairy_home.html")
+    userForm = UserForm.objects.all()
+    dict = {}
+    for user in userForm:
+        item_list = user.item.split(",")
+        for item in item_list:
+            if item in dict:
+                dict[item] += 1
+            else:
+                dict[item] = 1
+
+    print(dict)
+    return render(request, "Dairy/dairy_home.html", {'dict': dict})
 
 
 def login_user(request):
